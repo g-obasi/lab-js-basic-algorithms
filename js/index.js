f8effbbb1d7098ac2eda0049cbe884536ae967d9
Greenfield Obasi St Jean <gstjean@andrew.cmu.edu>
2023-12-02 17:08:38 +0200

    completed first commit for assignment

--- js/index.js
+++ js/index.js
@@ -1,6 +1,44 @@
 // Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
 
 
-// Iteration 3: Loops
+let rock = 'rock';
+let scissors = 'scissors';
+let paper = 'paper';
+
+// these two variables will be changed to test the conditional
+let firstPlayerChoice;
+let secondPlayerChoice;
+
+let firstPossibility;
+let secondPossibility;
+let thirdPossibility;
+
+let fourthPossibility;
+let fifthPossibility;
+let sixthPossibility;
+
+let seventhPossibility;
+
+// All possible scenarios where one player will win
+firstPossibility = firstPlayerChoice === paper && secondPlayerChoice === rock;
+secondPossibility = firstPlayerChoice === rock && secondPlayerChoice === scissors;
+thirdPossibility = firstPlayerChoice === scissors && secondPlayerChoice === paper;
+
+fourthPossibility = secondPlayerChoice === paper && firstPlayerChoice === rock;
+fifthPossibility = secondPlayerChoice === rock && firstPlayerChoice === scissors;
+sixthPossibility = secondPlayerChoice === scissors && firstPlayerChoice === paper;
+
+// Scenario where both choices are the same, resulting in a tie
+seventhPossibility = firstPlayerChoice === secondPlayerChoice;
+
+// Conditional statement
+if (firstPossibility || secondPossibility || thirdPossibility) {
+    console.log("First player won!");
+} else if (fourthPossibility || fifthPossibility || sixthPossibility) {
+    console.log("Second player won!");
+} else if (seventhPossibility) {
+    console.log("It's a tie!");
+} else {
+    console.log("Something went wrong!");
+}
+
